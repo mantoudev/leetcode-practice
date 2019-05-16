914eef1e798325e6563eefb501b70948544ff074
mantoudev <mantoudev@163.com>
2019-05-16 20:45:40 +0800

    update 139
    Signed-off-by: mantoudev <mantoudev@163.com>

--- 01_LeetCode/139. Word Break/Solution.java
+++ 01_LeetCode/139. Word Break/Solution.java
@@ -5,17 +5,18 @@ class Solution {
         boolean[] result = new boolean[len + 1];
         // 最大单词长度
         for (String temp : wordDict){
-            if (temp.length() > max_len)
-                max_len = temp.length();
+          max_len = temp.length() > max_len ? temp.length() : max_len;
         }
+        result[0] = true;
         // 遍历字符串, 从第一个字母开始，依次往后，判断是否可拆分
-        for (int i = 1; i < len; i++){
+        for (int i = 1; i <= len; i++){
             // 从 j 到 i 是否可拆分
             for (int j = i - 1; j >= 0 && i - j <= max_len; j--){
                //1. 如果0-j不可拆分，j-i也不可拆分 ； 2. 如果0-j可拆分，判断j-i是否包含在字典中
-                if (result[j] && wordDict.contains(s.substring(j, i)))
+                if (result[j] && wordDict.contains(s.substring(j, i))){
                     result[i] = true;
                     break;
+                  }
             }
         }
         return result[len];
