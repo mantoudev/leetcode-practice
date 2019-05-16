bdc760c0532c3af681965f7a67243ab7ee229d64
mantoudev <mantoudev@163.com>
2019-05-16 16:26:20 +0800

    add 344 in java
    Signed-off-by: mantoudev <mantoudev@163.com>

---
+++ 01_LeetCode/344. Reverse String/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        char temp;
+        while(l < r){
+            temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
